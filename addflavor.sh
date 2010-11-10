2277cea001552c90442315641ce321aa24050bbc
Koert van der Veer <koert@openpanel.com>
2010-11-10 09:19:11 +0100

    Eradicated most references to PanelSix

--- addflavor.sh
+++ addflavor.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# This file is part of OpenPanel - The Open Source Control Panel
+# OpenPanel is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, using version 3 of the License.
+#
+# Please note that use of the OpenPanel trademark may be subject to additional 
+# restrictions. For more information, please visit the Legal Information 
+# section of the OpenPanel website on http://www.openpanel.com/
+
 inflav=`cat makeinclude | grep ^CXXFLAGS | sed -e "s/.*__FLAVOR_//;s/ .*//"`
 if [ "$inflav" = "LINUX_REDHAT" ]; then
  cp rsrc/openpanel.module.awstats.conf.xml.redhat rsrc/openpanel.module.awstats.conf.xml
