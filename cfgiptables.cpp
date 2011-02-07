fddbfa5d17e2ad874bbb41a3a4e5061f2d190492
Koert van der Veer <koert@openpanel.com>
2011-02-07 13:02:15 +0100

    Fixed ipv6 firewalling

--- cfgiptables.cpp
+++ cfgiptables.cpp
@@ -192,7 +192,7 @@ void cfgiptables::buildfromval (const value &v)
 
 	// * Add global policy	
 	out += "$IPTABLES -I INPUT -p icmp -j ACCEPT\n";
-	out += "$IP6TABLES -I INPUT -p icmp -j ACCEPT\n";
+	out += "$IP6TABLES -I INPUT -p icmpv6 -j ACCEPT\n";
 	out += "$IPTABLES -I INPUT -m state --state RELATED,ESTABLISHED "
 		   "-j ACCEPT\n";
 	out += "$IP6TABLES -I INPUT -m state --state RELATED,ESTABLISHED "
